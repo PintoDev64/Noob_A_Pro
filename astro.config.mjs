a4a39fa95db89c6bbd085f737e7b524255e8883b
PintoGamer64 <pintogamer64@gmail.com>
2023-10-05 14:42:29 -0500

    Low fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import nodejs from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server'
+    adapter: nodejs({
+        mode: 'middleware' // or 'standalone'
+    }),
+    output: 'hybrid',
 });
