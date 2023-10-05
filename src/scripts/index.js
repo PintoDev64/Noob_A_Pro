32451cd8eee007990f7ea3ed522b871eb4044d6d
PintoGamer64 <pintogamer64@gmail.com>
2023-10-05 18:21:59 -0500

    Add View Transitions
    Se añadio el View Transitions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,29 @@
-const videosBackground =
-    document.getElementById("VideosBackground");
+const videosBackground = document.getElementById("VideosBackground");
 const Video = document.querySelectorAll(".video");
-const VideoSnippet =
-    document.getElementById("VideoInformation");
+const VideoSnippet = document.getElementById("VideoInformation");
+const VideoURL = document.getElementById("VideoURL");
 
 let posX,
     posY,
     activePoppover = false;
 
 const onEnter = (ev) => {
+    console.log(ev.target.childNodes[0].id);
     ev.preventDefault();
     if (!activePoppover) {
         activePoppover = true;
         posX = ev.clientX;
         posY = ev.clientY;
+        VideoURL.setAttribute("href", `https://www.youtube.com/watch?v=${ev.target.childNodes[0].id}`)
         VideoSnippet.style.display = "block";
-        VideoSnippet.classList.add('selected')
     } else {
         activePoppover = false;
         VideoSnippet.style.display = "none";
-        VideoSnippet.classList.remove('selected')
     }
 
     if (
-        posX + VideoSnippet.clientWidth + 30 <
-        videosBackground.clientWidth
+        posX + VideoSnippet.offsetWidth + 30 <
+        videosBackground.offsetWidth
     ) {
         VideoSnippet.style.left = `${posX}px`;
         VideoSnippet.style.top = `${posY}px`;
